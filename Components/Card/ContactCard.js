bfdd025a8752dc8ba65420ca8c24d898e8044034
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-17 16:07:25 +0200

    front-end

--- Components/Card/ContactCard.js
+++ Components/Card/ContactCard.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
 
-const ContactCard = () => {
+const ContactCard = ({ item }) => {
+  const name =
+    item.name.length > 25 ? item.name.substring(0, 20) + "..." : item.name;
   return (
-    <View>
-      <Text>ContactCard</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.outline}
+      //   onPress={() => {
+      //     setSpecChat(item);
+      //     setNavPage(APP_PAGES.APP.SPEC_CHAT);
+      //   }}
+    >
+      <View style={styles.grid}>
+        {/* <Image source={{ uri: item.image }} style={styles.image} /> */}
+        <View style={styles.info}>
+          <Text style={styles.name}>{name}</Text>
+          <Text>{item.phoneNumber}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 export default ContactCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  outline: {
+    marginVertical: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+  },
+  text: {
+    color: "#404040",
+    fontWeight: "500",
+    fontSize: 18,
+    paddingHorizontal: 10,
+  },
+});
--- Components/Views/ContactView.js
+++ Components/Views/ContactView.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import React from "react";
+import { AppContext } from "../../context/AppContext";
+import ContactCard from "../Card/ContactCard";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
 const ContactView = () => {
+  const { contacts, isLoading, setIsLoading } = React.useContext(AppContext);
   return (
     <View style={styles.outline}>
       <Text style={styles.title}>Contacts</Text>
-      <ScrollView></ScrollView>
+      <FlatList
+        data={contacts}
+        renderItem={({ item }) => <ContactCard item={item} />}
+        keyExtractor={(item) => `${item.id}-${item.name}`}
+        initialNumToRender={10}
+        onEndReachedThreshold={0.5}
+        maxToRenderPerBatch={10}
+        onEndReached={() => {
+          setIsLoading(true);
+          // fetch more data
+          setIsLoading(false);
+        }}
+        ListFooterComponent={
+          isLoading ? (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+          ) : null
+        }
+      />
     </View>
   );
 };
